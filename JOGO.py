c89b101ae3a616b4b9b127f13059de54bf96dd32
Victor Freitas <victorf.santos@outlook.com>
2018-06-22 12:39:35 -0300

    Pequenas modificacoes em JOGO.py

--- JOGO.py
+++ JOGO.py
@@ -9,17 +9,24 @@ def escolha_jogo():
 
     print("(1) Forca ou (2) Adivinhacao\n")
 
-    jogo = int(input("Qual Jogo vc deseja jogar?"))
+    repetir = "S"
 
-    while jogo != 1 and jogo != 2:
-        jogo = int(input("Informe uma opcao valida"))
+    while repetir == "S":
 
-    if jogo == 1:
-        print("\nVoce escolheu Forca\n\n")
-        Forca.jogar()
-    elif jogo == 2:
-        print("\nVoce escolheu Adivinhacao\n\n")
-        Adivinhacao.jogar()
+        jogo = int(input("Qual Jogo vc deseja jogar?"))
+
+        while jogo != 1 and jogo != 2:
+            jogo = int(input("Informe uma opcao valida"))
+
+        if jogo == 1:
+            print("\nVoce escolheu Forca\n\n")
+            Forca.jogar()
+        elif jogo == 2:
+            print("\nVoce escolheu Adivinhacao\n\n")
+            Adivinhacao.jogar()
+
+        repetir = (input("Deseja jogar novamente? S/N")).upper().strip()
+
 
 
 if __name__ == "__main__":
