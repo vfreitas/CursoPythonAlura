ec5aa433e85f981abe93a42877c39bb27d08563a
Victor Freitas <victorf.santos@outlook.com>
2018-06-22 12:31:12 -0300

    Adicionei funcoes ao arquivo Advinhacao.py

--- Adivinhacao.py
+++ Adivinhacao.py
@@ -1,14 +1,18 @@
 import random
 
 
-def jogar():
+def imprime_cabecalho():
     print("***************************\n"
           "--- JOGO DA ADIVINHACAO ---\n"
           "***************************\n")
 
-    numero_secreto = round(random.randrange(1,101))
-    pontos = 100
 
+def define_numero_secreto():
+    numero_secreto = round(random.randrange(1, 101))
+    return numero_secreto
+
+
+def define_nivel():
     nivel = int(input("\nQual o nivel de dificuldade??\n1 - Facil\n2 - Medio\n3 - Dificil\n"))
 
     if nivel == 1:
@@ -20,10 +24,42 @@ def jogar():
     elif nivel == 3:
         total_de_tentativas = 3
 
+    return total_de_tentativas
+
+
+def finaliza_jogo(numero_secreto, pontos):
+    print("\n\nFim de jogo")
+    print("O numero secreto era {}".format(numero_secreto))
+    print("Voce terminou com {:03} pontos".format(pontos))
+
+
+def perca_de_pontos(maior, menor, chute, numero_secreto, pontos):
+    if maior:
+        print("\nVoce errou")
+        print("O numero secreto eh maior que", chute)
+        pontos = pontos - (numero_secreto - chute)
+
+    elif menor:
+        print("\nVoce errou")
+        print("O numero secreto eh menor que", chute)
+        pontos = pontos - (chute - numero_secreto)
+
+    return pontos
+
+
+def jogar():
+    imprime_cabecalho()
+
+    numero_secreto = define_numero_secreto()
+    pontos = 100
+
+    total_de_tentativas = define_nivel()
+
+
     for rodada in range(1,total_de_tentativas + 1):
         print("\n\nTentativa {} de {}".format(rodada, total_de_tentativas))
         chute = int(input("\nTente adivinhar o numero secreto:"))
-        
+
         while chute < 1 or chute > 100:
             print("Numero Invlido")
             chute = int(input("Informe um numero entre  e 100: "))
@@ -36,19 +72,9 @@ def jogar():
             print("\nVoce acertou")
             break
 
-        elif maior:
-            print("\nVoce errou")
-            print("O numero secreto eh maior que", chute)
-            pontos = pontos - (numero_secreto - chute)
+        pontos = perca_de_pontos(maior, menor, chute, numero_secreto, pontos)
 
-        elif menor:
-            print("\nVoce errou")
-            print("O numero secreto eh menor que", chute)
-            pontos = pontos - (chute - numero_secreto)
-
-    print("\n\nFim de jogo")
-    print("O numero secreto era {}".format(numero_secreto))
-    print("Voce terminou com {:03} pontos".format(pontos))
+    finaliza_jogo(numero_secreto, pontos)
 
 if __name__ == "__main__":
     jogar()
