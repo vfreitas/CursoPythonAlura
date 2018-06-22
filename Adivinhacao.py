a721d3c68f284b3beead0be2b779b533566508d2
Victor Freitas <victorf.santos@outlook.com>
2018-06-22 00:02:39 -0300

    So arrumando estetica de codigo

--- Adivinhacao.py
+++ Adivinhacao.py
@@ -11,18 +11,20 @@ def jogar():
 
     nivel = int(input("\nQual o nivel de dificuldade??\n1 - Facil\n2 - Medio\n3 - Dificil\n"))
 
-    if (nivel == 1):
+    if nivel == 1:
         total_de_tentativas = 10
-    elif (nivel == 2):
+
+    elif nivel == 2:
         total_de_tentativas = 5
-    elif (nivel == 3):
+
+    elif nivel == 3:
         total_de_tentativas = 3
 
     for rodada in range(1,total_de_tentativas + 1):
         print("\n\nTentativa {} de {}".format(rodada, total_de_tentativas))
-
         chute = int(input("\nTente adivinhar o numero secreto:"))
-        while (chute < 1) or (chute > 100):
+        
+        while chute < 1 or chute > 100:
             print("Numero Invlido")
             chute = int(input("Informe um numero entre  e 100: "))
 
@@ -30,14 +32,16 @@ def jogar():
         maior = numero_secreto > chute
         menor = numero_secreto < chute
 
-        if (acertou):
+        if acertou:
             print("\nVoce acertou")
             break
-        elif (maior):
+
+        elif maior:
             print("\nVoce errou")
             print("O numero secreto eh maior que", chute)
             pontos = pontos - (numero_secreto - chute)
-        elif (menor):
+
+        elif menor:
             print("\nVoce errou")
             print("O numero secreto eh menor que", chute)
             pontos = pontos - (chute - numero_secreto)
@@ -46,6 +50,6 @@ def jogar():
     print("O numero secreto era {}".format(numero_secreto))
     print("Voce terminou com {:03} pontos".format(pontos))
 
-if (__name__ == "__main__"):
+if __name__ == "__main__":
     jogar()
 
--- JOGO.py
+++ JOGO.py
@@ -1,25 +1,26 @@
 import Forca
 import Adivinhacao
 
+
 def escolha_jogo():
-    print("**************\n"
-          "Escolha o jogo\n"
-          "**************\n")
+    print("**************************\n"
+          "----  ESCOLHA O JOGO -----\n"
+          "**************************\n")
 
     print("(1) Forca ou (2) Adivinhacao\n")
 
     jogo = int(input("Qual Jogo vc deseja jogar?"))
 
-    while(jogo!=1 and jogo!=2):
+    while jogo != 1 and jogo != 2:
         jogo = int(input("Informe uma opcao valida"))
 
-    if (jogo == 1):
+    if jogo == 1:
         print("\nVoce escolheu Forca\n\n")
         Forca.jogar()
-    elif (jogo == 2):
+    elif jogo == 2:
         print("\nVoce escolheu Adivinhacao\n\n")
         Adivinhacao.jogar()
 
 
-if (__name__ == "__main__"):
+if __name__ == "__main__":
     escolha_jogo()
